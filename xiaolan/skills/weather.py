85cdc2e4f3900fe207b8a8016965f17424ea0f38
Lanzhijiang <37663413+xiaoland@users.noreply.github.com>
2018-03-24 11:37:42 +0800

    Update weather.py

--- xiaolan/skills/weather.py
+++ xiaolan/skills/weather.py
@@ -16,6 +16,20 @@ def start():
 
 def main():
     
-    URL = ''
-    data = { }
-    r = requests.post(URL, data)
+    API: '9f4785b7a627c847408230423d34787b'
+    URL = 'https://api.darksky.net/forecast/'
+    long = '113.197281'
+    lati = '22.658797'
+    lang = 'zh'
+    r = requests.get(URL + '/' + API + '/' + long + ',' + lati)
+    summary = r.json()['summary']
+    temperature = r.json()['temperature']
+    temperatureHigh = r.json()['temperatureHigh']
+    icon = r.json()['icon']
+    humidity = r.json()['humidity']
+    temperatureLow = r.json()['temperatureLow']
+    if icon = 'rain':
+        icon = '下雨'
+    stext = ("今天最高温为", temperatureHigh, "最低温为：", temperatureLow, "天气状态是是：", icon)
+    baidu_tts.tts(stext)
+    speaker.say()
