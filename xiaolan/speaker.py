74641392897e26a8987567af7b4135dd5d5e4ed7
Lanzhijiang <37663413+xiaoland@users.noreply.github.com>
2018-03-24 12:07:24 +0800

    Update speaker.py

--- xiaolan/speaker.py
+++ xiaolan/speaker.py
@@ -7,13 +7,13 @@ import pygame
 import hyper
 
 def ding(): #开始录制指令提示音
-    os.system('omxplayer /home/pi/xiaolan/musiclib/ding.wav')
+    os.system('omxplayer /home/pi/xiaolan/xiaolan/musiclib/ding.wav')
 
 def dong(): #结束录制指令提示音
-    os.system('omxplayer /home/pi/xiaolan/musiclib/dong.wav')
+    os.system('omxplayer /home/pi/xiaolan/xiaolan/musiclib/dong.wav')
 
 def poweroff(): #关机提示音（太懒，mp3文件还没弄）
-    os.system('omxplayer /home/pi/xiaolan/musiclib/poweroff.mp3')
+    os.system('omxplayer /home/pi/xiaolan/musiclib/xiaolan/poweroff.mp3')
 
 def say(): #说出的回话
-    os.system('omxplayer /home/pi/xiaolan/say.mp3')
+    os.system('omxplayer /home/pi/xiaolan/xiaolan/say.mp3')
