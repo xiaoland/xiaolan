5e74d428266172e066cb0b8b900294d8fdd55939
Lanzhijiang <37663413+xiaoland@users.noreply.github.com>
2018-03-31 11:04:07 +0800

    Update clock.py

--- xiaolan/skills/clock.py
+++ xiaolan/skills/clock.py
@@ -5,6 +5,7 @@ import sys
 import os
 import requests
 import pygame
+import time
 sys.path.append('/home/pi/xiaolan/xiaolan/')
 import stt
 import tts
@@ -13,8 +14,21 @@ import recorder
 import speaker
 
 def start(d, re, redata, h, m):
+    
     main(d, re, redata, h, m)
   
 def main(d, re, redata, h, m):
     if re == 'True':
+        flag = 1
+        while flag:
+            t = time.localtime()  # 当前时间的纪元值
+            fmt = "%H %M"
+            now = time.strftime(fmt, t)  # 将纪元值转化为包含时、分的字符串
+            now = now.split(' ') #以空格切割，将时、分放入名为now的列表中
+
+            hour = now[0]
+            minute = now[1]
+            if hour == h and minute == m:
+                os.sys(omxplayer /home/pi/xiaolan/xiaolan/music/clock.mp3)
+                flag = 0
         
