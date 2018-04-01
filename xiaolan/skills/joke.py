328fd52e3eba0a90bcca724270e0368965c03711
Lanzhijiang <37663413+xiaoland@users.noreply.github.com>
2018-04-01 20:29:22 +0800

    Update and rename story.py to joke.py

---
+++ xiaolan/skills/joke.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+''' 故事/笑话 '''
+import json
+import sys
+import os
+import requests
+import pygame
+import time
+sys.path.append('/home/pi/xiaolan/xiaolan/')
+import stt
+import tts
+import snowboy
+import recorder
+import speaker
+
+def start():
+    
+    main()
+    
+def main():
+    
+    url = 'http://api.avatardata.cn/Joke/QueryJokeByTime?key='
+    key = 'a63ac25e95f741aea51167a05891498c'
+    r = requests.post(url + key + '&page=2&rows=10&sort=asc&time=1418745237')
+    joke = r.json()['content']
+    baidu_tts(joke)
+    speaker.speak()
