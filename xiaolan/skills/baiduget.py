d258797be49373936d1e0c581698637e418ccd0b
Lanzhijiang <37663413+xiaoland@users.noreply.github.com>
2018-04-01 15:46:23 +0800

    Update baiduget.py

--- xiaolan/skills/baiduget.py
+++ xiaolan/skills/baiduget.py
@@ -1,1 +1,18 @@
+import sys
+import os
+import requests
+import json
+sys.path.append('/home/pi/xiaolan/xiaolan/')
+import stt
+import tts
+import snowboy
+import speaker
+import recorder
 
+def start(ser):
+  
+    main(ser)
+    
+def main(ser):
+    
+    
