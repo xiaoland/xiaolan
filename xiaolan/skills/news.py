a0780fc82c96b7b1da2e300dab069591d8ce7a87
Lanzhijiang <37663413+xiaoland@users.noreply.github.com>
2018-04-02 20:48:36 +0800

    Update news.py

--- xiaolan/skills/news.py
+++ xiaolan/skills/news.py
@@ -20,17 +20,25 @@ def start(newsservice):
 def main(newsservice):
   cAPIKEY = '549dfe30dba14d87bcbca16c86bc4d85'
   nAPIKEY = 'e32ca74cc1ec40dba642f07ffc529447'
+  kAPIKEY = '37d6e1142ceb4cbe98b5cb2de55300d2'
   chinanewsurl = 'http://api.avatardata.cn/GuoNeiNews/Query?key='
-  nationnews = 'http://api.avatardata.cn/WorldNews/Query?key='
+  nationnewsurl = 'http://api.avatardata.cn/WorldNews/Query?key='
+  kejinewsurl = 'http://api.avatardata.cn/TechNews/Query?key='
     if newsservice = 'chinanews':
         r = requests.post(chinanewsurl + cAPIKEY + '&page=1&rows=1')
         saytext = r.json()['title']
         baidu_tts.tts(saytext)
         speaker.speak()
+        
     elif newsservice = 'nationnews':
         r = requests.post(nationnewsurl + nAPIKEY + '&page=1&rows=1')
         saytext = r.json()['title']
         baidu_tts.tts(saytext)
         speaker.speak()
     
+    elif newsservice = 'kejinews':
+        r = requests.post(kejinewsurl + kAPIKEY + '&page=1&rows=1')
+        saytext = r.json()['title']
+        baidu_tts.tts(saytext)
+        speaker.speak()
     
