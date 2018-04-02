8dafe79ec2c957463c637ec2a36340b2b8fa46ab
Lanzhijiang <37663413+xiaoland@users.noreply.github.com>
2018-04-02 20:42:08 +0800

    Update speaker.py

--- xiaolan/speaker.py
+++ xiaolan/speaker.py
@@ -15,5 +15,8 @@ def dong(): #结束录制指令提示音
 def poweroff(): #关机提示音（太懒，mp3文件还没弄）
     os.system('omxplayer /home/pi/xiaolan/musiclib/xiaolan/poweroff.mp3')
 
+def kacha(): #拍照声
+    os.system('omxplayer /home/pi/xiaolan/xiaolan/musiclib/kacha.mp3')
+    
 def speak(): #说出的回话
     os.system('omxplayer /home/pi/xiaolan/xiaolan/say.mp3')
