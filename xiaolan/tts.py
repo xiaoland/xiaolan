e08e6145dae2feec30e323cbf716c1857268c4c7
Lanzhijiang <37663413+xiaoland@users.noreply.github.com>
2018-04-03 23:10:59 +0800

    Update tts.py

--- xiaolan/tts.py
+++ xiaolan/tts.py
@@ -9,22 +9,18 @@ import xlpath
 import json
 import pyaudio
 import time
-import tempfile
-import sys
-reload(sys)
-sys.setdefaultencoding("utf-8")
+import shutil  
+from urllib import quote  
 
 
 class baidu_tts(object):
-    def __init__(self, stext, say, token, turl, AK, SK, url, per):
-        self.stext = saytext
-        self.say = say
+    def __init__(self, token, turl, AK, SK, url, per):
         self.token = '24.b889fe13015f30355edcdd5343fc2ee9.2592000.1523371315.282335-10747354'
         self.turl = 'http://openapi.baidu.com/oauth/2.0/token'
         self.url = 'http://tsn.baidu.com/text2audio'
         self.AK = 'M9jz0511Yfbb15d1BshqtC5g'
         self.SK = 'Z73I2jvytEa8QydGnNlP3oOKfudIlvgE'
-        self.per = '4'
+        self.per = 4
         
     def get_token(self):
         try:
@@ -53,10 +49,7 @@ class baidu_tts(object):
                                   exc_info=True)
           
         
-    def tts(self):
-        if self.token == '':
-            self.token = self.get_token()
-        punctuations = ['.', '。', ';', '；', '\n']
+    def tts(self, saytext):
         if self.token == '':
             self.token = self.get_token()
         query = {'tex': self.stext,
@@ -68,16 +61,8 @@ class baidu_tts(object):
                  }
         r = requests.post('http://tsn.baidu.com/text2audio',
                           data=query,
-                          headers={'content-type': 'application/json'})    
-        try:
-            r.raise_for_status()
-            if r.json()['err_msg'] is not None:
-                print ('Baidu TTS failed with response: %r',
-                                      r.json()['err_msg'])
-                return None
-        except Exception:
-            pass
-        with tempfile.NamedTemporaryFile(suffix='.mp3', delete=False) as f:
-            f.write(r.content)
-            say = f.name
-            return say
+                          headers={'content-type': 'application/json'})
+        if r.status_code == 200:
+            with open(r"home/pi/xiaolan/xiaolan/musiclib/say.mp3", 'wb') as f:
+                r.raw.decode_content = True
+                shutil.copyfileobj(r.raw, f)
