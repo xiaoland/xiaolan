182d48a58abcf918397eea29da3dca06dfb48014
Lanzhijiang <37663413+xiaoland@users.noreply.github.com>
2018-04-04 23:26:15 +0800

    Update music.py

--- xiaolan/skills/music.py
+++ xiaolan/skills/music.py
@@ -37,58 +37,58 @@ def main(name):
 
     for value in mm:
 
-    url = 'http://sug.music.baidu.com/info/suggestion'
-    payload = {'word': value, 'version': '2', 'from': '0'}
-    print "Song Name: " + value
+    	url = 'http://sug.music.baidu.com/info/suggestion'
+    	payload = {'word': value, 'version': '2', 'from': '0'}
+    	print "Song Name: " + value
 
-    r = requests.get(url, params=payload)
-    contents = r.text
-    d = json.loads(contents, encoding="utf-8")
-    if('data' not in d):
-    	print "do not have flac\n"
-        continue
-    if('song' not in d["data"]):
-    	print "do not have flac\n"
-    	continue
-    songid = d["data"]["song"][0]["songid"]
-    print "Song ID: " + songid 
+    	r = requests.get(url, params=payload)
+    	contents = r.text
+    	d = json.loads(contents, encoding="utf-8")
+    	if('data' not in d):
+    		print "do not have flac\n"
+        	continue
+    	if('song' not in d["data"]):
+    		print "do not have flac\n"
+    		continue
+    	songid = d["data"]["song"][0]["songid"]
+    	print "Song ID: " + songid 
 
-    url = "http://music.baidu.com/data/music/fmlink"
-    payload = {'songIds': songid, 'type': 'mp3'}
-    r = requests.get(url, params=payload)
-    contents = r.text
-    d = json.loads(contents, encoding="utf-8")
-    if d is not None and 'data' not in d or d['data'] == '':
-        continue
-    songlink = d["data"]["songList"][0]["songLink"]
-    if(len(songlink) < 10):
-        print "do not have flac\n"
-        continue
-	print "Song Source: " + songlink + "\n";
+    	url = "http://music.baidu.com/data/music/fmlink"
+    	payload = {'songIds': songid, 'type': 'mp3'}
+    	r = requests.get(url, params=payload)
+    	contents = r.text
+    	d = json.loads(contents, encoding="utf-8")
+    	if d is not None and 'data' not in d or d['data'] == '':
+        	continue
+    	songlink = d["data"]["songList"][0]["songLink"]
+    	if(len(songlink) < 10):
+        	print "do not have flac\n"
+        	continue
+		print "Song Source: " + songlink + "\n";
 
-    songdir = "songs"
-    if not os.path.exists(songdir):
-        os.makedirs(songdir)
+    	songdir = "songs"
+   	if not os.path.exists(songdir):
+        	os.makedirs(songdir)
 
-    songname = d["data"]["songList"][0]["songName"]
-    artistName = d["data"]["songList"][0]["artistName"]
-    filename = "music.flac"
+    	songname = d["data"]["songList"][0]["songName"]
+    	artistName = d["data"]["songList"][0]["artistName"]
+    	filename = "music.flac"
 
-    f = urllib2.urlopen(songlink)
-    headers = requests.head(songlink).headers
-    size = int(headers['Content-Length']) / (1024 ** 2)
+   	f = urllib2.urlopen(songlink)
+    	headers = requests.head(songlink).headers
+    	size = int(headers['Content-Length']) / (1024 ** 2)
 
-    if not os.path.isfile(filename) or os.path.getsize(filename) < minimumsize:
-        print "%s is downloading now ......\n" % songname
-        with open(filename, "wb") as code:
+    	if not os.path.isfile(filename) or os.path.getsize(filename) < minimumsize:
+        	print "%s is downloading now ......\n" % songname
+        	with open(filename, "wb") as code:
                 code.write(f.read())
-    else:
-        print "%s is already downloaded. Finding next song...\n\n" % songname
-    print "\n================================================================\n"
-    print "Download finish!"
-    filename = '/home/pi/xiaolan/xiaolan/musiclib/music.flac'
-    flac_to_mp3.chageFile(filename)
-    speaker.player()
+    	else:
+        	print "%s is already downloaded. Finding next song...\n\n" % songname
+    		print "\n================================================================\n"
+    		print "Download finish!"
+    		filename = '/home/pi/xiaolan/xiaolan/musiclib/music.flac'
+    		flac_to_mp3.chageFile(filename)
+    		speaker.player()
         
     
     
