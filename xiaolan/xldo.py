d1f04b09f71d17d1fdfa656e8fdec73257192c7f
Lanzhijiang <37663413+xiaoland@users.noreply.github.com>
2018-04-04 13:26:01 +0800

    Update xldo.py

--- xiaolan/xldo.py
+++ xiaolan/xldo.py
@@ -55,39 +55,19 @@ def welcome():
 
 def snowboystart():
     
-    def signal_handler(signal, frame):
-        global interrupted
-        interrupted = True
-
-
-    def interrupt_callback():
-        global interrupted
-        return interrupted
-
-    model = '/home/pi/xiaolan/xiaolan/snowboy/xiaolanxiaolan.pmdl'
-    
-    # capture SIGINT signal, e.g., Ctrl+C
-    signal.signal(signal.SIGINT, signal_handler)
-
-    detector = snowboydecoder.HotwordDetector(model, sensitivity=0.5)
-    print('Listening... Press Ctrl+C to exit')
-
-    # main loop
-    detector.start(detected_callback=convenstation,
-                   interrupt_check=interrupt_callback,
-                   sleep_time=0.03)
-
-    detector.terminate()
+    snowboy.start()
 
 def convenstation():
 
+    snowboystop()
     time.sleep(1)
     speaker.ding()
     recorder.record()
     speaker.dong()
     baidu_stt.stt()
-    intent = witch
-
+    query = witch
+    skills.getskills(witch)
+    
 class skills(object):
     
     def __init__(self, witch):
