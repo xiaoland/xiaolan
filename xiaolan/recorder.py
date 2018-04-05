e64fd3056a1bd4a8834774549bd844bd1df21d4d
Lanzhijiang <37663413+xiaoland@users.noreply.github.com>
2018-04-05 12:01:12 +0800

    Update recorder.py

--- xiaolan/recorder.py
+++ xiaolan/recorder.py
@@ -3,10 +3,8 @@
 
 import wave, pyaudio
 import numpy as np
-import requests, json
 import sys
 import os
-import snowboy
 
 
 CHUNK = 1024
@@ -16,29 +14,33 @@ CHANNELS = 1
 RECORD_SECONDS = 10
 WAVE_OUTPUT_FILENAME = "voice.wav"
 
-def record(): # 录制指令（这里没有问题）
-    snowboy.stop()
-    p = pyaudio.PyAudio()
-    stream = p.open(format = FORMAT,
-                    channels = CHANNELS,
-                    rate = RATE,
-                    input = True,
-                    frames_per_buffer=CHUNK)
-    print "* recording"
-    frames = []
-    for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
-        data = stream.read(CHUNK)
-        frames.append(data)
+class recorder(object):
+    def __init__(self):
+        
+        pass
+    
+    def record(): # 录制指令（这里没有问题）
+        p = pyaudio.PyAudio()
+        stream = p.open(format = FORMAT,
+                        channels = CHANNELS,
+                        rate = RATE,
+                        input = True,
+                        frames_per_buffer=CHUNK)
+        print "* recording"
+        frames = []
+        for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
+            data = stream.read(CHUNK)
+            frames.append(data)
 
-    print "* done recording"
+        print "* done recording"
 
-    stream.stop_stream()
-    stream.close()
-    p.terminate()
+        stream.stop_stream()
+        stream.close()
+        p.terminate()
 
-    wf = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
-    wf.setnchannels(CHANNELS)
-    wf.setsampwidth(p.get_sample_size(FORMAT))
-    wf.setframerate(RATE)
-    wf.writeframes(b''.join(frames))
-    wf.close()
+        wf = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
+        wf.setnchannels(CHANNELS)
+        wf.setsampwidth(p.get_sample_size(FORMAT))
+        wf.setframerate(RATE)
+        wf.writeframes(b''.join(frames))
+        wf.close()
