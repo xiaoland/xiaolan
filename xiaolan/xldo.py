35bdb4beeac6b57a1bd28ed460f09b5c536f0d6a
Lanzhijiang <37663413+xiaoland@users.noreply.github.com>
2018-04-07 20:26:06 +0800

    Update xldo.py

--- xiaolan/xldo.py
+++ xiaolan/xldo.py
@@ -67,7 +67,7 @@ def convenstation():
     speaker.ding()
     r.record()
     speaker.dong()
-    tok = b.get_token(1)
+    tok = b.get_token()
     text = b.stt('./voice.wav', tok)
     query = b.nlpquery(text)
     s.getskills(query)
