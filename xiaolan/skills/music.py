43ba71a731af1307e23e4bdab6011beb7bab6738
Lanzhijiang <37663413+xiaoland@users.noreply.github.com>
2018-04-07 22:44:30 +0800

    Update music.py

--- xiaolan/skills/music.py
+++ xiaolan/skills/music.py
@@ -12,8 +12,8 @@ import re
 import urllib2
 from Crypto.Cipher import AES
 sys.path.append('/home/pi/xiaolan/xiaolan/')
-import stt
-import tts
+from stt import bsidu_stt
+from tts import baidu_tts
 import snowboy
 import speaker
 import recorder
