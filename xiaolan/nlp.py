b588a511468ffcf936eff298dbccbb7212a417ac
Lanzhijiang <37663413+xiaoland@users.noreply.github.com>
2018-04-09 23:19:42 +0800

    Update nlp.py

--- xiaolan/nlp.py
+++ xiaolan/nlp.py
@@ -6,8 +6,11 @@ import requests
 import pygame
 import demjson
 import time
+from recorder import recorder
+import snowboy
+from tts import baidu_tts
+from stt import baidu_stt
 
-intent = ''
 
 def get_intent(text):
         ak = 'cd3c2238c7348d28363a1aad0b93d474'
@@ -27,26 +30,45 @@ def get_intent(text):
         return domain
         
         
-def do_intent(domian, json)
-        if intent == '':
-            if domain == 'ticket':
-                semantic = json['semantic']
-                transportation = semantic['transportation']
-                start_loc = semantic['start_loc']
-                startdate = semantic['startdate']
-                return startdate, start_loc, transportation
-            elif domain == 'weather':
-                semantic = json['semantic']
-                city = semantic['city']
-                return city
-            elif domain == 'music':
-                semantic = json['semantic']
-                mode = semantic['mode']
-                return mode
-            elif domain == 'joke':
-                sence = 'joke'
-                return sence
-            else:
-                tuling = 'tuling'
-                return tuling
-        return domian
+def do_intent(text): #自制的语义理解系统,欢迎大家补充
+        if '闹钟' in text:
+                intent = 'clock'
+                return intent
+        elif '打开' in text:
+                intent = 'smarthome'
+                return intent
+        elif '翻译' in text:
+                intent = 'ts'
+                return intent
+        elif '搜索' in text:
+                intent = 'ser'
+                return intent
+        elif '闲聊' in text:
+                intent = 'tuling'
+                return intent
+        elif '关机' in text:
+                print ('SHUTDOWING...')
+                os.system('sudo poweroff')
+        elif '重启' in text:
+                print ('REBOOTING...')
+                os.system('sudo reboot')
+        elif '怎么走' in text:
+                intent = 'map'
+                return intent
+        elif '酒店' in text:
+                intent = 'hotel'
+                return intent
+        elif '旅游' in text:
+                intent = 'travel'
+                return intent
+        elif '做游戏' in text:
+                intent = 'minigame'
+                return intent
+        elif '新闻' in text:
+                intent = 'news'
+                return intent
+        elif '傻逼' in text:
+                speaker.falu()
+        else:
+                intent = 'tuling'
+                return intent
