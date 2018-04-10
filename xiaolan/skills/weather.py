4b0eeeae0ca3fd137529c83a199776ce9e7363ec
Lanzhijiang <37663413+xiaoland@users.noreply.github.com>
2018-04-10 13:37:11 +0800

    Update weather.py

--- xiaolan/skills/weather.py
+++ xiaolan/skills/weather.py
@@ -7,7 +7,7 @@ import json
 import pygame
 import requests
 sys.path.append('/home/pi/xiaolan/xiaolan/')
-from stt import bsidu_stt
+from stt import baidu_stt
 from tts import baidu_tts
 import snowboy
 import speaker
