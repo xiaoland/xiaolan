f29b81a7cbd29c3e5d61d1072713e4065e9dcf2f
Lanzhijiang <37663413+xiaoland@users.noreply.github.com>
2018-04-12 21:16:27 +0800

    Update xlonly.py

--- xiaolan/skills/xlonly.py
+++ xiaolan/skills/xlonly.py
@@ -13,15 +13,22 @@ import recorder
 import speaker
 
 def xlonly():
+    
+    saytext1 = '老爸，老妈，你们好啊，我是你们儿女派来的私人助理，小蓝'
+    saytext3 = '有什么吩咐吗？'
 
-    baidu_tts.tts("老爸，老妈，你们好啊，我是你们儿女派来的私人助理，小蓝")
-    speaker.say()
-    baidu_tts.tts("有什么吩咐吗？")
-    speaker.say()
+    saytext2 = saytext1.encode('utf-8','strict')
+    saytext4 = saytext3.encode('utf-8','strict')
+    bt = baidu_tts()
+    tok = bt.get_token()
+    bt.tts(saytext2, tok)
+    speaker.speak()
+    bt.tts(saytext4, tok)
+    speaker.speak()
     speaker.ding()
     recorder.record()
     speaker.dong()
-    baidu_stt.stt()
+    bs = baidu_stt(1, token, 2, '{')
     
     
     
