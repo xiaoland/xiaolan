177b1ca991a81978dac9ba51808ed5296675c329
Lanzhijiang <37663413+xiaoland@users.noreply.github.com>
2018-04-21 21:26:43 +0800

    Update music.py

--- xiaolan/skills/music.py
+++ xiaolan/skills/music.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 
-import re
 import time
 import sys
 import os
@@ -11,7 +10,6 @@ import eyed3
 import subprocess
 import hashlib
 import re
-import subprocess
 import threading
 from MusicBoxApi import api as NetEaseApi
 sys.path.append('/home/pi/xiaolan/xiaolan/')
@@ -29,22 +27,5 @@ speaker.ding()
 r.record()
 speaker.dong()
 text = bs.stt('./voice.wav', tok)
-
-if '随机' in text:
-    netease = NetEaseApi.NetEase()
-    music_list = netease.top_songlist()
-    datalist = netease.dig_info(music_list, 'songs')
-    playlist = []
-    for data in datalist:
-        music_info = {}
-        music_info.setdefautl("song_name", data.get("song_name"))
-        music_info.setdefault("artist", data.get("artist"))
-        music_info.setdefault("album_name", data.get("album_name"))
-        music_info.setdefault("mp3_url", data.get("mp3_url"))
-        music_info.setdefault("playTime", data.get("playTime"))
-        music_info.setdefault("quality", data.get("quality"))
-        playlist.append(music_info)
-    print playlist
-    return playlist
     
     
