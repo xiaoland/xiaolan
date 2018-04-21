29eb291d9b88c6610a38e92574bb509d5e4dddfe
Lanzhijiang <37663413+xiaoland@users.noreply.github.com>
2018-04-21 20:05:48 +0800

    Update speaker.py

--- xiaolan/speaker.py
+++ xiaolan/speaker.py
@@ -2,26 +2,39 @@
 # 音响控制器
 import sys
 import os
-import mp3player
+import logging
+import pygame
+import hyper
 
 def ding(): #开始录制指令提示音
-    os.system('omxplayer /home/pi/xiaolan/xiaolan/musiclib/ding.wav')
+        os.system('omxplayer /home/pi/xiaolan/xiaolan/musiclib/ding.wav')
 
 def dong(): #结束录制指令提示音
+    
     os.system('omxplayer /home/pi/xiaolan/xiaolan/musiclib/dong.wav')
 
 def poweroff(): #关机提示音（太懒，mp3文件还没弄）
+    
     os.system('omxplayer /home/pi/xiaolan/musiclib/xiaolan/poweroff.mp3')
 
 def kacha(): #拍照声
+    
     os.system('omxplayer /home/pi/xiaolan/xiaolan/musiclib/kacha.mp3')
     
 def speak(): #说出的回话
     
-    mp3player.playMP3('/home/pi/xiaolan/xiaolan/musiclib/say.mp3')
+    pygame.init()
+    pygame.mixer.music.load('/home/pi/xiaolan/xiaolan/musiclib/say.mp3')
+    pygame.mixer.music.play()
     
-def player(): #音乐播放器
-    os.system('omxplayer /home/pi/xiaolan/xiaolan/music.mp3')
+def music(): #音乐播放器
     
-def speacilrecorder(): #重新录制
-    os.system('omxplayer /home/pi/xiaolan/xiaolan/musiclib/speacilrecorder.mp3')
+    pygame.init()
+    pygame.mixer.music.load('/home/pi/xiaolan/xiaolan/musiclib/music.mp3')
+    pygame.mixer.music.play()
+    
+def speacilrecorder():
+    
+    pygame.init()
+    pygame.mixer.music.load('omxplayer /home/pi/xiaolan/xiaolan/musiclib/speacilrecorder.mp3')
+    pygame.mixer.music.play()
