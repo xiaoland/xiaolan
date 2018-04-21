8bbfbe18eff6e23b0b8a317c463b647fa19884d9
Lanzhijiang <37663413+xiaoland@users.noreply.github.com>
2018-04-21 19:57:39 +0800

    Update music.py

--- xiaolan/skills/music.py
+++ xiaolan/skills/music.py
@@ -31,14 +31,13 @@ speaker.dong()
 text = bs.stt('./voice.wav', tok)
 
 if '随机' in text:
-    def get_top_songlist():
     netease = NetEaseApi.NetEase()
     music_list = netease.top_songlist()
     datalist = netease.dig_info(music_list, 'songs')
     playlist = []
     for data in datalist:
         music_info = {}
-        music_info.setdefault("song_name", data.get("song_name"))
+        music_info.setdefautl("song_name", data.get("song_name"))
         music_info.setdefault("artist", data.get("artist"))
         music_info.setdefault("album_name", data.get("album_name"))
         music_info.setdefault("mp3_url", data.get("mp3_url"))
