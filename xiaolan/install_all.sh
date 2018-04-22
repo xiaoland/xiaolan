b950946b704bd2c00492382b361373b823d5d7a1
Lanzhijiang <37663413+xiaoland@users.noreply.github.com>
2018-04-22 21:23:59 +0800

    Update install_all.sh

--- xiaolan/install_all.sh
+++ xiaolan/install_all.sh
@@ -6,6 +6,8 @@ pip install pyaudio
 pip install requests hyper crypto
 sudo apt-get install libatlas-base-dev
 mv /home/pi/xiaolan-dev /home/pi/xiaolan
+echo "请输入root账号的密码："
+echo "如果输入完毕之后，停止了运行，在本脚本文件里找到本行，往下数第二行一直拖到最底复制带命令行中执行"
 su root
 cp /home/pi/xiaolan/xiaolan/autostartxl /etc/init.d/
 chmod +777 /etc/init.d/autostartxl
