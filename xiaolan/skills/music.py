4534682a6d12b9e8972225f07f7cebcdd964f74b
Lanzhijiang <37663413+xiaoland@users.noreply.github.com>
2018-04-22 11:01:22 +0800

    Update music.py

--- xiaolan/skills/music.py
+++ xiaolan/skills/music.py
@@ -6,11 +6,6 @@ import os
 import requests
 import json
 import demjson
-import subprocess
-import hashlib
-import re
-import threading
-from MusicBoxApi import api as NetEaseApi
 sys.path.append('/home/pi/xiaolan/xiaolan/')
 import speaker
 from recorder import recorder
@@ -20,11 +15,21 @@ from tts import baidu_tts
 bt = baidu_tts()
 bs = baidu_stt(1, 'a', 2, '{')
 r = recorder()
-tok = bs.get_token()
-asktext = '欢迎使用小蓝音乐播放器，请输入指令，是随机播放还是搜索'
-speaker.ding()
-r.record()
-speaker.dong()
-text = bs.stt('./voice.wav', tok)
+
+def start():
+    m = music()
+    tok = bs.get_token()
+    m.musicwelcome(tok)
+
+class music(object):
+    def __init__(self):
+        pass
+    def musicwelcome(self, tok):
+
+        asktext = '欢迎使用小蓝音乐播放器，请输入指令，是随机播放还是搜索'
+        speaker.ding()
+        r.record()
+        speaker.dong()
+        text = bs.stt('./voice.wav', tok)
     
     
