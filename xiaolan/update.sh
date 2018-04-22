992f269c182fc95da5764006a497f288df8ed891
Lanzhijiang <37663413+xiaoland@users.noreply.github.com>
2018-04-22 12:23:19 +0800

    Update and rename update.sh to xiaolan-trouble_fix-download.sh

--- xiaolan/update.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-echo "更新小蓝中..."
-cd /home/pi
-git pull master
-cd /home/pi
-git clone
---
+++ xiaolan/xiaolan-trouble_fix-download.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+echo "更新小蓝中..."
+cd /home/pi
+git clone https://github.com/xiaoland/xiaolan-trouble_fix.git
