b4e2eef57cb8f352205cdba074f0482b1672e753
Lanzhijiang <37663413+xiaoland@users.noreply.github.com>
2018-04-22 10:22:17 +0800

    Update install_all.sh

--- xiaolan/install_all.sh
+++ xiaolan/install_all.sh
@@ -4,7 +4,7 @@ sudo apt-get install cmake g++ gcc python2.7 python3
 sudo apt-get install python-dev python3-dev python-pyaudio python3-pyaudio sox
 su root
 pip install pyaudio
-pip install python-requests python-demjson
+pip install demjson requests
 pip install MusicBoxApi==1.0.4
 sudo apt-get install libatlas-base-dev
 mv /home/pi/xiaolan-dev /home/pi/xiaolan
