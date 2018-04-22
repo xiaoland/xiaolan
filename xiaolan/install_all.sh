11b33ca0cc244e5c5e8be93182e9467d044586a4
Lanzhijiang <37663413+xiaoland@users.noreply.github.com>
2018-04-22 10:16:24 +0800

    Update install_all.sh

--- xiaolan/install_all.sh
+++ xiaolan/install_all.sh
@@ -3,10 +3,9 @@ sudo apt-get install omxplayer mplayer pulseaudio fswebcam
 sudo apt-get install cmake g++ gcc python2.7 python3
 sudo apt-get install python-dev python3-dev python-pyaudio python3-pyaudio sox
 su root
-pip install pyaudio jieba
-pip install python3-requests python-requests python-demjson python3-demjson
+pip install pyaudio
+pip install python-requests python-demjson
 pip install MusicBoxApi==1.0.4
-su pi
 sudo apt-get install libatlas-base-dev
 mv /home/pi/xiaolan-dev /home/pi/xiaolan
 cp /home/pi/xiaolan/xiaolan/autostartxl /etc/init.d/
