f2321f0438aca6c4c8fa9109c4852810c7fbf025
Lanzhijiang <37663413+xiaoland@users.noreply.github.com>
2018-04-25 22:15:02 +0800

    Update recorder.py

--- xiaolan/recorder.py
+++ xiaolan/recorder.py
@@ -13,6 +13,7 @@ RATE = 16000
 CHANNELS = 1
 RECORD_SECONDS = 5
 SRECORD_SECONDS = 4
+TRECORD_SECONDS = 12
 WAVE_OUTPUT_FILENAME = "voice.wav"
 TRAIN_F = "train_f.wav"
 TRAIN_S = "train_s.wav"
@@ -121,6 +122,32 @@ class recorder(object):
         p.terminate()
 
         wf = wave.open(TRAIN_T, 'wb')
+        wf.setnchannels(CHANNELS)
+        wf.setsampwidth(p.get_sample_size(FORMAT))
+        wf.setframerate(RATE)
+        wf.writeframes(b''.join(frames))
+        wf.close()
+        
+    def tsrecord(self): # 录制指令（这里没有问题）
+        p = pyaudio.PyAudio()
+        stream = p.open(format = FORMAT,
+                        channels = CHANNELS,
+                        rate = RATE,
+                        input = True,
+                        frames_per_buffer=CHUNK)
+        print "* recording"
+        frames = []
+        for i in range(0, int(RATE / CHUNK * TRECORD_SECONDS)):
+            data = stream.read(CHUNK)
+            frames.append(data)
+
+        print "* done recording"
+
+        stream.stop_stream()
+        stream.close()
+        p.terminate()
+
+        wf = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
         wf.setnchannels(CHANNELS)
         wf.setsampwidth(p.get_sample_size(FORMAT))
         wf.setframerate(RATE)
