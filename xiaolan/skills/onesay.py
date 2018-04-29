8d6bea709fbe9636012fa12a0e2859b0d9f84428
Lanzhijiang <37663413+xiaoland@users.noreply.github.com>
2018-04-30 00:14:03 +0800

    Update onesay.py

--- xiaolan/skills/onesay.py
+++ xiaolan/skills/onesay.py
@@ -1,1 +1,45 @@
+# -*- coding: UTF-8 -*-
+import sys
+import base64
+import requests
+import os
+import json
+import demjson
+import base64
+import hashlib
+import music
+sys.path.append('/home/pi/xiaolan/xiaolan/')
+import speaker
+from stt import baidu_stt
+from tts import baidu_tts
+import snowboy
+from recorder import recorder
 
+def start(text):
+    if '打开' in text:
+        switch = text[2:-1]
+        hassmode = 'turn_on'
+        smarthome.onesay(switch, hassmode)
+    elif '关闭' in text:
+        switch = text[2:-1]
+        hassmode = 'turn_off'
+        smarthome.onesay(switch, hassmode)
+    elif '播放' in text:
+        if '音乐' in text:
+            music.start()
+        else:
+            songname = text[2:-1]
+            music.onesay(songname)
+    elif '我想听' in text:
+        if '音乐' in text:
+            music.start()
+        else:
+            songname = text[3:-1]
+            music.onesay(songname)
+    elif '儿子' in text:
+        xlonly.ei()
+        xlonly.start()
+        
+          
+            
+        
