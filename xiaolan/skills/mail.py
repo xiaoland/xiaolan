5e1a2d77c6263cca381c22a64c9edf594a6955af
Lanzhijiang <37663413+xiaoland@users.noreply.github.com>
2018-05-20 22:57:52 +0800

    Create mail.py

---
+++ xiaolan/skills/mail.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+import time
+import sys
+import os
+import requests
+import json
+import demjson
+import random
+sys.path.append('/home/pi/xiaolan/xiaolan/')
+import speaker
+from recorder import recorder
+from stt import baidu_stt
+from tts import baidu_tts
