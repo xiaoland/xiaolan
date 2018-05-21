f5ba8b6cab7a2f9da022f365b2167e14e1865b27
Lanzhijiang <37663413+xiaoland@users.noreply.github.com>
2018-05-21 13:27:19 +0800

    Update install_all.sh

--- xiaolan/install_all.sh
+++ xiaolan/install_all.sh
@@ -2,7 +2,7 @@
 sudo apt-get install omxplayer pulseaudio fswebcam
 sudo apt-get install cmake g++ gcc python2.7 python3
 sudo apt-get install python-dev python3-dev python-pyaudio python3-pyaudio sox
-pip install pyaudio
+pip install pyaudio sendmail
 pip install requests hyper crypto
 sudo apt-get install libatlas-base-dev
 mv /home/pi/xiaolan-dev /home/pi/xiaolan
