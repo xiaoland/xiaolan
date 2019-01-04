6354bb1d4af90ec0f00c0a0ab0e82330663baa67
xiaoland <1481605673@qq.com>
2019-01-05 05:47:35 +0800

    joke skill update

--- xiaolan/skills/joke.py
+++ xiaolan/skills/joke.py
@@ -13,45 +13,46 @@ from tts import baidu_tts
 import recorder
 import speaker
 
+
 def start(tok):
     
     main(tok)
-    
+
+
 def main(tok):
     
-    url = 'http://api.avatardata.cn/Joke/QueryJokeByTime?key='
-    key = 'a63ac25e95f741aea51167a05891498c'
+    url = 'http://v.juhe.cn/joke/content/list.php'
+    key = '21e780488098fa8b77394405421d8ae1'
     bt = baidu_tts()
     bs = baidu_stt(1, 'a', 2, '{')
-    chose = random.randint(0,9)
+    chose = random.randint(0, 9)
     
-    r = requests.post(url + key + '&sort=asc&time=1418745237')
+    r = requests.get(url + "key=" + key + "&page=2&pagesize=10&sort=asc&time=1418745237")
     
     json = r.json()
     
     if chose == 0:
-        saytext = json['result'][0]['content']
+        saytext = json['result']["data"][0]['content']
     elif chose == 1:
-        saytext = json['result'][1]['content']
+        saytext = json['result']["data"][1]['content']
     elif chose == 2:
-        saytext = json['result'][2]['content']
+        saytext = json['result']["data"][2]['content']
     elif chose == 3:
-        saytext = json['result'][3]['content']
+        saytext = json['result']["data"][3]['content']
     elif chose == 4:
-        saytext = json['result'][4]['content']
+        saytext = json['result']["data"][4]['content']
     elif chose == 5:
-        saytext = json['result'][5]['content']
+        saytext = json['result']["data"][5]['content']
     elif chose == 6:
-        saytext = json['result'][6]['content']
+        saytext = json['result']["data"][6]['content']
     elif chose == 7:
-        saytext = json['result'][7]['content']
+        saytext = json['result']["data"][7]['content']
     elif chose == 8:
-        saytext = json['result'][8]['content']
+        saytext = json['result']["data"][8]['content']
     elif chose == 9:
-        saytext = json['result'][9]['content']
+        saytext = json['result']["data"][9]['content']
     else:
-        saytext = json['result'][0]['content']
-        
-    saytext = jokefj + '第二个笑话' + jokesj
+        saytext = json['result']["data"][0]['content']
+
     bt.tts(saytext, tok)
     speaker.speak()
