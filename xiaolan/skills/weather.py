cc86e36d8f5af59aaf2208e1ac2f76846ff22d8e
xiaoland <1481605673@qq.com>
2019-01-05 05:30:03 +0800

    weather skill update

--- xiaolan/skills/weather.py
+++ xiaolan/skills/weather.py
@@ -2,28 +2,32 @@
 '''天气'''
 import sys
 import os
-import logging
 import json
-import pygame
 import requests
-import urllib2
-import re
-import socket
+import random
 sys.path.append('/home/pi/xiaolan/xiaolan/')
 from stt import baidu_stt
 from tts import baidu_tts
 import speaker
 from recorder import recorder
 
+saytext_list = [
+    "今天，s，温度是，d，摄氏度",
+    "你猜啊！算了，告诉你吧：今天s，温度大概是d摄氏度",
+    "我生气了，不想告诉你。但是看在你这么可爱的份上，告诉你咯：今天s，温度d摄氏度",
+    "我是你的可爱小蓝，天气这就来：今天s，温度d摄氏度",
+    "今天s，温度d摄氏度，天气天天都有，但如果我没有就恐怖了"
+]
+
+
 def start(tok):
 
     main(tok)
 
+
 def main(tok):
 
     bt = baidu_tts()
-    bs = baidu_stt(1, 'a', 2, '{')
-    r = recorder()
     host = 'https://api.seniverse.com/v3/weather/now.json?key='
     key = 'sxyi6ehxblxkqeto'
     APIURL = key + '&location=ip&language=zh-Hans&unit=c'
@@ -37,6 +41,8 @@ def main(tok):
     weather = json['results'][0]['now']['text']
     temperature = json['results'][0]['now']['temperature']
     
-    tweatherstates = ',今天,' + weather + '，温度是,'  + temperature + '，摄氏度，'
+    saytext = saytext_list[random.randint(0, 4)]
+    saytext.replace("s", weather)
+    saytext.replace("d", temperature)
     bt.tts(saytext, tok)
     speaker.speak()
