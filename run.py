294a3dd3ac72c90411ae6476a2c1dc64d4e5e603
Lanzhijiang <lanzhijiang@foxmail.com>
2020-10-05 08:13:31 +0800

    「更新」新增对话

--- run.py
+++ run.py
@@ -4,8 +4,16 @@
 # date: 2020/10/1
 
 import json
+import os
+from snowboy.snowboy import XiaolanSnowboy
 from log import XiaolanLog
+from recorder import XiaolanRecorder
+from player import XiaolanPlayer
+from nlu import XiaolanNlu
+from skill_manager import XiaolanSkills
 from tts import BaiduTts
+from stt import BaiduStt
+
 
 class XiaolanInit():
 
@@ -13,7 +21,14 @@ class XiaolanInit():
 
         self.setting = json.load(open("./data/json/setting.json", "r", encoding="utf-8"))
         self.log = XiaolanLog()
+
         self.tts = BaiduTts(self.log, self.setting)
+        self.stt = BaiduStt(self.log, self.setting)
+        self.nlu = XiaolanNlu(self.log, self.setting)
+        self.player = XiaolanPlayer(self.log, self.setting)
+        self.recorder = XiaolanRecorder(self.log, self.setting)
+        self.skills = XiaolanSkills(self.log, self.setting)
+        self.snowboy = XiaolanSnowboy(self.log, self.setting)
 
     def run(self):
 
@@ -34,5 +49,47 @@ class XiaolanInit():
         self.log.add_log("Xiaolan start! ", 1)
         self.log.add_log("Self checking...", 1)
 
-        self.tts.start("你好呀，主人！~ 这里是小蓝")
+        self.player.welcome()
 
+        self.log.add_log("Start pulseaudio", 1)
+        os.system("pulseaudio --start")
+
+        self.log.add_log("Run snowboy awaken engine", 1)
+        self.snowboy.run(self.callback)
+
+    def callback(self):
+
+        """
+        回调函数
+        :return:
+        """
+        self.log.add_log("XiaolanInit: Detected awaken from snowboy", 1)
+        self.conversation()
+
+    def conversation(self):
+
+        """
+        开始对话
+        :return:
+        """
+        self.log.add_log("XiaolanInit: Start new conversation", 1)
+
+        self.player.ding()
+        self.recorder.record()
+        self.player.dong()
+
+        text = self.stt.start()
+        if text is None:
+            self.log.add_log("XiaolanInit: text result null", 3)
+            self.player.text_none()
+            return
+        else:
+            intent = self.nlu.get_intent(text)
+            if intent[0] is None:
+                self.player.intent_none()
+                return
+            else:
+                if intent[0] == "call_skill":
+                    self.skills.skill_skills_list[intent[1]](self.log, self.setting, text)
+                else:
+                    self.skills.intent_skills_list[intent[0]](self.log, self.setting, text)
