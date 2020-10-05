ac6deaef809dc17f1637638ad7956202811d74ea
Lanzhijiang <lanzhijiang@foxmail.com>
2020-10-05 23:48:14 +0800

    「更新」hass更新

--- skills/hass.py
+++ skills/hass.py
@@ -4,6 +4,7 @@
 # date: 2020/10/5
 
 import requests
+import json
 from stt import BaiduStt
 from tts import BaiduTts
 from nlu import XiaolanNlu
@@ -21,6 +22,7 @@ class XiaolanSkillHass():
 
         self.stt = BaiduStt(log, setting)
         self.tts = BaiduTts(log, setting)
+        self.nlu = XiaolanNlu(log, setting)
         self.recorder = XiaolanRecorder(log, setting)
         self.player = XiaolanPlayer(log, setting)
 
@@ -33,11 +35,26 @@ class XiaolanSkillHass():
             "Content-Type": "application/json"
         }
 
-        self.components = []
-        self.unit_system = {}
-        self.domain_services = {}
-        self.entity_dict = {}
-        self.friendly_name_dict = {}
+        self.components = self.hass_settings["components"]
+        self.unit_system = self.hass_settings["unitSystem"]
+        self.domain_services = self.hass_settings["domainServices"]
+        self.entity_dict = self.hass_settings["entityDict"]
+        self.friendly_name_dict = self.hass_settings["friendlyNameDict"]
+        self.intent_service_list = self.hass_settings["intentToServiceList"]
+
+        self.nlu_intent_dict = {
+            "更新数据": "update_data",
+            "打开": "turn_on",
+            "关闭": "turn_off",
+            "颜色": "set_color",
+            "查看": "get_state",
+            "设置": "set",
+            "改": "set",
+            "获取": "get_state",
+            "是": "yes",
+            "否": "no",
+            "不": "no"
+        }
 
     def start(self):
 
@@ -45,6 +62,62 @@ class XiaolanSkillHass():
         开始
         :return:
         """
+        self.log.add_log("XiaolanSkillHass: Initializing...", 1)
+
+        self.tts.start("Hi!~ 这里是智能家居控制系统~")
+
+        self.main()
+
+    def conversation(self):
+
+        """
+        对话（减少重复）
+        :return:
+        """
+        self.player.ding()
+        self.recorder.record()
+        self.player.dong()
+
+        text = self.stt.start()
+
+        intent =  self.nlu.skill_nlu(text, self.nlu_intent_dict)
+        if intent is None:
+            self.log.add_log("XiaolanSkillHass: Intent is none, quit", 1)
+            self.tts.start("不好意思，我没明白，退出咯~")
+            return None
+        return intent, text
+
+    def main(self):
+
+        """
+        主逻辑程序（可多次对话）
+        :return:
+        """
+        self.tts.start("有何贵干呐？敬请吩咐。若不明白如何操作，请查阅本技能的wiki")
+
+        intent, text = self.conversation()
+        if intent is None:
+            return
+
+        self.log.add_log("XiaolanSkillHass: Recognized intent: " + intent, 1)
+
+        if intent == "update_data":
+            self.update_data()
+            self.tts.start("所有数据已更新")
+        elif intent == "get_state":
+            friendly_name = text[2:]
+            state = self.get_state(self.friendly_name_dict[friendly_name])
+            self.tts.start(friendly_name + "的状态是" + state["state"].replace("_", " "))
+
+        self.tts.start("还要继续吗？")
+
+        intent, text = self.conversation()
+        if intent is None:
+            return
+        if intent == "yes":
+            self.main()
+        else:
+            self.tts.start("好的，我走啦，下次再见~")
 
     def request(self, url, type, param=None):
 
@@ -73,12 +146,36 @@ class XiaolanSkillHass():
             self.log.add_log("    Res: " + str(r.text), 1)
             return None
 
+    def update_data(self):
+
+        """
+        更新所有数据
+        :return:
+        """
+        self.log.add_log("XiaolanSkillHass: Update all data...", 1)
+        self.get_services()
+        self.get_entity_list()
+        self.get_config()
+
+        self.hass_settings["components"] = self.components
+        self.hass_settings["unitSystem"] = self.unit_system
+        self.hass_settings["domainServices"] = self.domain_services
+        self.hass_settings["entityDict"] = self.entity_dict
+        self.hass_settings["friendlyNameDict"] = self.friendly_name_dict
+
+        self.setting["hassSettings"] = self.hass_settings
+
+        json.dump(self.setting, open("./data/json/setting.json", "w", encoding="utf-8"))
+
+        self.log.add_log("XiaolanSkillHass: Updated", 1)
+
     def get_config(self):
 
         """
         获取hass的配置
         :return:
         """
+        self.log.add_log("XiaolanSkillHass: Get config", 1)
         r = self.request(self.hass_addr+"/api/config",
                          "get")
         if r is not None:
