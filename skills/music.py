8e142a311242ca60ff6b12a5640367cd3941176e
Lanzhijiang <lanzhijiang@foxmail.com>
2020-10-06 17:03:19 +0800

    「更新」重写music player

--- skills/music.py
+++ skills/music.py
@@ -1,233 +1,182 @@
-# -*- coding: utf-8 -*-
+# coding=utf-8
+# author: Lan_zhijiang
+# description: XiaolanSkill: music player
+# date: 2020/10/6
 
-import sys
-import os
 import requests
-import random
-sys.path.append('/home/pi/old_xiaolan/old_xiaolan/')
-from xiaolan import speaker
-from recorder import recorder
-from stt import baidu_stt
-from tts import baidu_tts
+import urllib.parse
+from stt import BaiduStt
+from tts import BaiduTts
+from player import XiaolanPlayer
+from recorder import XiaolanRecorder
+from nlu import XiaolanNlu
 
-def start(tok):
-    m = xlMusic()
-    m.main(tok)
 
-class xlMusic(object):
+class XiaolanSkillMusicPlayer(object):
     
-    def __init__(self):
+    def __init__(self, log, setting, text):
         
-        pass
-    
-    def command_choose(self, commands, tok):
-        
-        if '随机播放' in commands or '随机' in commands:
-            command = 'sui_ji'
-            return command
-        elif '搜索播放' in commands or '搜索' in commands:
-            command = 'sou_suo'
-            return command
-        elif '歌名' in commands or '这首歌叫什么' in commands:
-            command = 'song_name_q'
-            return command
-        elif '退出' in commands:
-            command = 'exit'
-            return command
-        else:
-            command = 're'
-            return command
-    
-    def paly(self, song_name, tok):
-        
-        bt = baidu_tts()
-        bs = baidu_stt(1, 2, 3, 4)
-        r = recorder()
-        m = xlMusic()
-        
-        while song_name != None:
-            speaker.play()
-            bt.tts('请问还要听什么吗？可以输入指令，如，这首歌叫什么，或者，搜索歌曲', tok)
-            speaker.speak()
-            speaker.ding()
-            r.record()
-            speaker.dong()
-            commands = bs.stt('./voice.wav', tok)
-            command = m.command_choose(commands, tok)
-            if command == 'sui_ji':
-                m.sui_ji(services, tok)
-            elif command == 'sou_suo':
-                m.sou_suo(services, tok)
-            elif command == 'exit':
-                bt.tts('谢谢使用，下次再见', tok)
-                speaker.speak()
-                os.system('python /home/pi/old_xiaolan/old_xiaolan/xldo.py b')
-                break
-            elif command == 're':
-                bt.tts('对不起，我没有听清楚您说了什么？', tok)
-                speaker.speak()
-                m.main(tok)
-        else:
-            bt.tts('对不起，发生了故障', tok)
-            speaker.speak()
-    
-    def sou_suo(self, services, song_name, tok):
-        
-        bt = baidu_tts()
-        bs = baidu_stt(1, 2, 3, 4)
-        r = recorder()
-        m = xlMusic()
-        
-        url = 'http://tingapi.ting.baidu.com/v1/restserver/ting?'
-        
-        if song_name == '' or song_name == None:
-            bt.tts('请问您要听什么歌?', tok)
-            speaker.speak()
-            speaker.ding()
-            r.record()
-            speaker.dong()
-            song_name = bs.stt('./voice.wav', tok)
-        else:
-            get_song_id_rawj = requests.get(url + services['search'] + song_name)
-            get_song_id_j = get_song_id_rawj.json()
-            try:
-                id = get_song_id_j['song'][song_name_c]['songid']
-            except KeyError:
-                try:
-                    id = get_song_id_j['song'][0]['songid']
-                except KeyError:
-                    bt.tts('对不起，播放错误')
-                    speaker.speak()
-                else:
-                    pass
+        self.log = log
+        self.setting = setting
+        self.text = text
+
+        self.stt = BaiduStt(log, setting)
+        self.tts = BaiduTts(log, setting)
+        self.nlu = XiaolanNlu(log, setting)
+        self.player = XiaolanPlayer(log, setting)
+        self.recorder = XiaolanRecorder(log, setting)
+
+        self.play_list = {}
+
+        self.nlu_dict = {
+            "恢复": "resume",
+            "播放": "play",
+            "暂停": "pause",
+            "下一首": "next",
+            "上一首": "previous",
+            "添加": "add",
+            "退出": "quit"
+        }
+
+    def start(self):
+
+        """
+        启动
+        :return:
+        """
+        self.log.add_log("XiaolanSkillMusicPlayer: Start", 1)
+
+        self.tts.start("请问你要听什么音乐呢？")
+        self.main()
+
+    def conversation(self):
+
+        """
+        对话（减少重复代码）
+        :return:
+        """
+        self.player.ding()
+        self.recorder.record()
+        self.player.dong()
+
+        text = self.stt.start()
+
+        intent, slot = self.nlu.skill_nlu(text, self.nlu_dict)
+        if intent is None:
+            self.log.add_log("XiaolanSkillMusicPlayer: Intent is none, quit", 1)
+            self.tts.start("不好意思，我没明白，退出咯~")
+            intent, text, slot = None, None, None
+        self.log.add_log("XiaolanSkillMusicPlayer: Intent: " + intent + " Text: " + text
+                         + " Slot: " + slot, 1)
+        return intent, text, slot
+
+    def main(self):
+
+        """
+        主业务逻辑
+        :return:
+        """
+        intent, text, slot = self.conversation()
+
+        if intent == "quit":
+            self.tts.start("好的，那么bye~")
+            return
+        elif intent == "play":
+            result = self.search(slot)
+            if result is None:
+                self.tts.start("没有找到你要找的曲目，你可以试着换个说法")
+                self.main()
             else:
-                get_song_url_rawj = requests.get(url + services['musicurl_get'] + id)
-                get_song_url_j = get_song_url_rawj.json()
-                
-                song_name = get_song_url_j['songinfo']['title']
-                song_url_f = get_song_url_j['bitrate']['file_link']
-                song_url = (song_url_f.replace('\', '''))
-                
-                download = requests.get(song_url)
-                with open("/home/pi/old_xiaolan/old_xiaolan/musiclib/music.mp3", "wb") as code:
-                    code.write(download.content)
-                
-                m.play(song_name, tok)
-    
-    def sui_ji(self, services, tok):
-        
-        bt = baidu_tts()
-        bs = baidu_stt(1, 2, 3, 4)
-        r = recorder()
-        m = xlMusic()
-        
-        url = 'http://tingapi.ting.baidu.com/v1/restserver/ting?'
-        song_name_c = random.uniform(0, 12)
-        if song_name_c == 0:
-            song_name = '皮皮虾我们走'
-        elif song_name_c == 1:
-            song_name = 'I hope you think of me'
-        elif song_name_c == 2:
-            song_name = '小幸运'
-        elif song_name_c == 3:
-            song_name = '全部都是你'
-        elif song_name_c == 4:
-            song_name = '佛系少女'
-        elif song_name_c == 5:
-            song_name = 'Something just like this'
-        elif song_name_c == 6:
-            song_name = 'Feel this Moment'
-        elif song_name_c == 7:
-            song_name = 'Welcome to NewYork'
-        elif song_name_c == 8:
-            song_name = '洛天依投食歌'
-        elif song_name_c == 9:
-            song_name = '带你去旅行'
-        elif song_name_c == 10:
-            song_name = '死机之歌'
-        elif song_name_c == 11:
-            song_name = 'Shape of You'
-        elif song_name_c == 12:
-            song_name = 'Kiss Fight'
-        elif song_name_c == 13:
-            song_name = 'Hot song'
-        
-        if song_name != 'Hot song':
-            get_song_id_rawj = requests.get(url + services['search'] + song_name)
-        elif song_name == 'Hot song':
-            get_song_id_rawj = requests.get(url + services['hot'])
-        get_song_id_j = get_song_id_rawj.json()
-        
-        try:
-            if song_name == 'Hot song':
-                id = get_song_id_j['result']['list'][song_name_c]['song_id']
-            else:
-                id = get_song_id_j['song'][song_name_c]['songid']
-        except KeyError:
-            try:
-                if song_name == 'Hot song':
-                    id = get_song_id_j['result']['list'][song_name_c]['song_id']
-                else:
-                    id = get_song_id_j['song'][song_name_c]['songid']
-            except KeyError:
-                bt.tts('对不起，播放错误')
-                speaker.speak()
-            else:
-                pass
-        
+                self.play(result)
+
+    def play(self, data):
+
+        """
+        播放
+        :param data: 播放参数
+        :return:
+        """
+        self.log.add_log("XiaolanSkillMusicPlayer: Start play...", 1)
+
+        url = "https://wwwapi.kugou.com/yy/index.php?"
+        param = {
+            "r": "play/getdata",
+            "hash": data["fileHash"]
+        }
+        param = urllib.parse.urlencode(param)
+
+        r = requests.get(url+param)
+        if r.status_code == 200:
+            res = r.json()
+            play_url = res["data"]["play_url"].replace("\\\\", "")
+
+            self.log.add_log("XiaolanSkillMusicPlayer: Play url: " + play_url, 1)
+
+            r2 = requests.get(play_url)
+            file = open("./data/audio/music" + data["fileFormat"], "wb")
+            file.write(r2.content)
+            file.close()
+
+            self.player.format_converter("/data/audio/music"+data["fileFormat"], data["fileFormat"], ".wav")
+
+            self.player.play("./data/audio/music.wav")
         else:
-            get_song_url_rawj = requests.get(url + services['musicurl_get'] + id)
-            get_song_url_j = get_song_url_rawj.json()
-            
-            song_name = get_song_url_j['songinfo']['title']
-            song_url_f = get_song_url_j['bitrate']['file_link']
-            song_url = (song_url_f.replace('\', '''))
-            
-            download = requests.get(song_url)
-            with open("/home/pi/old_xiaolan/old_xiaolan/musiclib/music.mp3", "wb") as code:
-                code.write(download.content)
-            
-            m.play(song_name, tok)
-        
-    def main(self, tok):
-        
-        bt = baidu_tts()
-        bs = baidu_stt(1, 2, 3, 4)
-        r = recorder()
-        m = xlMusic()
-        
-        welcome = '欢迎使用小蓝音乐播放器，云服务使用百度音乐'
-        ask = '请问您要随机播放还是搜索播放？'
-        url = 'http://tingapi.ting.baidu.com/v1/restserver/ting?'
-        services = {'musicurl_get': 'method=baidu.ting.song.play&songid=', 'search': 'method=baidu.ting.search.catalogSug&query=', 'hot': 'method=baidu.ting.song.getRecommandSongList&song_id=877578&num=12'}
-        
-        bt.tts(welcome, tok)
-        speaker.speak()
-        bt.tts(ask, tok)
-        speaker.speak()
-        speaker.ding()
-        r.record()
-        speaker.dong()
-        
-        try:
-            commands = bs.stt('./voice.wav', tok)
-        except TypeError:
-            speaker.speak()
-            speaker.ding()
-            r.record()
-            speaker.dong()
-            command = m.command_choose(commands, tok)
+            self.log.add_log("XiaolanSkillMusicPlayer: Can't get: " + data["fileHash"] + " 's play url. Http error", 3)
+            return None
+
+    def search(self, keyword):
+
+        """
+        搜索曲目
+        :param keyword: 关键词
+        :return:
+        """
+        self.log.add_log("XiaolanSkillMusicPlayer: Search for song named: " + keyword, 1)
+
+        url = "https://complexsearch.kugou.com/v2/search/song?"
+
+        param = {
+            "callback": "callback123",
+            "keyword": keyword,
+            "page": 1,
+            "pagesize": 30,
+            "bitrate": 0,
+            "isfuzzy": 0,
+            "tag": "em",
+            "inputtype": 0,
+            "platform": "WebFilter",
+            "userid": -1,
+            "clientver": 2000,
+            "iscorrection": 1,
+            "privilege_filter": 0,
+            "srcappid": 2919,
+            "clienttime": 1601950702328,
+            "mid": 1601950702328,
+            "uuid": 1601950702328,
+            "dfid": "-",
+            "signature": "10C3F1C841892B24FD43DE172EB765EA"
+        }
+        param = urllib.parse.urlencode(param)
+
+        r = requests.get(url+param)
+        if r.status_code == 200:
+            res = r.json()
+            data = res["data"]["lists"]
+            file_format = "." + data[0]["HQExtName"]
+            song_name = data[0]["SongName"].replace("em>", "").replace("<", "").replace("/", "")
+            singer_name = data[0]["SingerName"]
+            file_hash = data[0]["HQFileHash"]
+
+            self.log.add_log("XiaolanSkillMusicPlayer: song: " + song_name + " singer: " + singer_name
+                             + " file hash: " + file_hash, 1)
+
+            return {
+                "fileFormat": file_format,
+                "fileHash": file_hash,
+                "songName": song_name,
+                "singerName": singer_name
+            }
         else:
-            if command == 'sui_ji':
-                m.sui_ji(services, tok)
-            elif command == 'sou_suo':
-                m.sou_suo(services, tok)
-            elif command == 'exit':
-                bt.tts('谢谢使用，下次再见', tok)
-                speaker.speak()
-                os.system('python /home/pi/old_xiaolan/old_xiaolan/xldo.py b')
-            elif command == 're':
-                bt.tts('对不起，我没有听清楚您说了什么？', tok)
-                speaker.speak()
-                m.main(tok)
-                
+            self.log.add_log("XiaolanSkillMusicPlayer: Search: " + keyword + " failed. Http error", 3)
+            return None
+
