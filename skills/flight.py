2df417f054e10dee82887c1b8a40be0f6f5821cf
Lanzhijiang <lanzhijiang@foxmail.com>
2020-12-12 21:58:06 +0800

    「更新」写一下...

--- skills/flight.py
+++ skills/flight.py
@@ -2,82 +2,112 @@
 import requests
 import json
 import sys
-sys.path.append('/home/pi/old_xiaolan/old_xiaolan/')
-from tts import baidu_tts
+import time
+from tts import BaiduTts
+from stt import BaiduStt
+from player import XiaolanPlayer
+from recorder import XiaolanRecorder
 
 
-def request_csairlines(dep, arr, date, cities_info_list, token):
+class XiaolanSkillFlightSearcher:
 
-    flight_list = {}
-    url = "https://b2c.csair.com/portal/flight/direct/query"
-    dep_code = cities_info_list[dep.decode()][0]
-    arr_code = cities_info_list[arr.decode()][0]
-    baidu_tts().tts("dep city code:", dep_code)
-    baidu_tts().tts("arr city code:", arr_code)
-    param = {
-        "depCity": dep_code,
-        "arrCity": arr_code,
-        "flightDate": str(date),
-        "adultNum": "1",
-        "childNum": "0",
-        "infantNum": "0",
-        "cabinOrder": "0",
-        "airLine": 1,
-        "flyType": 0,
-        "international": "0",
-        "action": "0",
-        "segType": "1",
-        "cache": 1,
-        "preUrl": "",
-        "isMember": ""
-    }
-    r = requests.post(url,
+    def __init__(self, log, setting, text):
+
+        self.log = log
+        self.setting = setting
+        self.text = text
+
+        self.tts = BaiduTts(log, setting)
+        self.stt = BaiduStt(log, setting)
+        self.recorder = XiaolanRecorder(log, setting)
+        self.player = XiaolanPlayer(log, setting)
+
+    def request_csairlines(self, dep, arr, date, cities_info_list, token):
+
+        flight_list = {}
+        url = "https://b2c.csair.com/portal/flight/direct/query"
+        dep_code = cities_info_list[dep.decode()][0]
+        arr_code = cities_info_list[arr.decode()][0]
+        self.log.add_log("Skill-FlightSearcher: dep city code: " + dep_code, 1)
+        self.log.add_log("Skill-FlightSearcher: arr city code: " + arr_code, 1)
+        param = {
+            "depCity": dep_code,
+            "arrCity": arr_code,
+            "flightDate": str(date),
+            "adultNum": "1",
+            "childNum": "0",
+            "infantNum": "0",
+            "cabinOrder": "0",
+            "airLine": 1,
+            "flyType": 0,
+            "international": "0",
+            "action": "0",
+            "segType": "1",
+            "cache": 1,
+            "preUrl": "",
+            "isMember": ""
+        }
+        r = requests.post(url,
                       data=json.dumps(param),
                       headers={"Content-Type": "application/json"})
-    res = r.json()
-    if res["success"] is True:
-        res_data = res["data"]["segment"][0]["dateFlight"]
-        for i in res_data["flight"]:
-            flight_list[i["flightNo"]] = i
-    else:
-        baidu_tts().tts("Error!!!FlightSearchFaild", token)
-        baidu_tts().tts("Error code:" + res["errorCode"], token)
-        baidu_tts().tts("Error msg:" + res["errorMsg"], token)
-        baidu_tts().tts(str(res))
-    return flight_list
+        res = r.json()
+        if res["success"] is True:
+            res_data = res["data"]["segment"][0]["dateFlight"]
+            for i in res_data["flight"]:
+                flight_list[i["flightNo"]] = i
+        else:
+            self.log.add_log("Skill-FlightSearcher: Flight search fail", 3)
+            self.log.add_log("    Error code:" + res["errorCode"], 3)
+            self.log.add_log("    Error msg:" + res["errorMsg"], 3)
+            self.log.add_log("    The response: \n" + str(res), 3)
+        return flight_list
 
 
-def get_cities_info_list():
+    def get_cities_info_list(self):
 
-    cities_info_list = {}
-    url = "https://www.csair.com/cn/scripts/city/getcitylist.html?callback=getB2CCmsCity&_=1564394534382"
-    r = requests.get(url)
-    content = r.content.decode().replace("getB2CCmsCity(", "").replace(")", "")
-    res = json.loads(content)
+        cities_info_list = {}
+        url = "https://www.csair.com/cn/scripts/city/getcitylist.html?callback=getB2CCmsCity&_=1564394534382"
+        r = requests.get(url)
+        content = r.content.decode().replace("getB2CCmsCity(", "").replace(")", "")
+        res = json.loads(content)
 
-    for i in res["cities"]:
-        cities_info_list[i[0]] = [i[4], i[11]]
-    return cities_info_list
+        for i in res["cities"]:
+            cities_info_list[i[0]] = [i[4], i[11]]
+        return cities_info_list
+    
+    def main(self):
 
+        self.tts.start("Welcome using the FlightTracer. Made by Lan_zhijiang")
+        self.tts.start("Loading cities... please wait...")
+        cities_info_list = self.get_cities_info_list()
+        self.tts.start("Load finished")
+        
+        self.tts.start("请问你要从哪里出发？请说出城市名")
+        self.player.ding()
+        self.recorder.record()
+        self.player.dong()
+        dep = self.stt.start()
 
-def main(token):
+        self.tts.start("请问你要到哪里去？请说出城市名")
+        self.player.ding()
+        self.recorder.record()
+        self.player.dong()
+        arr = self.stt.start()
 
-    baidu_tts().tts("Welcome using the FlightTracer")
-    baidu_tts().tts("Made by Lan_zhijiang")
-    baidu_tts().tts("Loading cities... please wait...")
-    cities_info_list = get_cities_info_list()
-    baidu_tts().tts("Load finished")
-    dep = "北京"
-    arr = "深圳"
-    date = "20190811"
+        date = self.log.get_date()
+        self.tts.start("请问你要什么时候出发？月份+日期，今天是" + date)
+        self.player.ding()
+        self.recorder.record()
+        self.player.dong()
+        date = "20190811"
 
-    baidu_tts().tts("Staring confirm info", token)
+        baidu_tts().tts("Staring confirm info", token)
 
-    baidu_tts().tts("Information confirm completed", token)
-    baidu_tts().tts("Searching for the flight...please wait...", token)
-    flight_list = request_csairlines(dep, arr, date, cities_info_list, token)
-    flight_num_list = list(flight_list.keys())
-    for i in flight_num_list:
-        dep_time = flight_list[i]["depTime"][0:2] + ":" + flight_list[i]["depTime"][2:4]
-        arr_time = flight_list[i]["arrTime"][0:2] + ":" + flight_list[i]["arrTime"][2:4]
-        baidu_tts().tts(i + "，这趟航班的经济舱价格是" + str(flight_list[i]["cabin"][-1]["adultPrice"]) + "，出发时间是" + str(dep_time) + "，到达时间是" + str(arr_time), token)
+        baidu_tts().tts("Information confirm completed", token)
+        baidu_tts().tts("Searching for the flight...please wait...", token)
+        flight_list = request_csairlines(dep, arr, date, cities_info_list, token)
+        flight_num_list = list(flight_list.keys())
+        for i in flight_num_list:
+            dep_time = flight_list[i]["depTime"][0:2] + ":" + flight_list[i]["depTime"][2:4]
+            arr_time = flight_list[i]["arrTime"][0:2] + ":" + flight_list[i]["arrTime"][2:4]
+            baidu_tts().tts(i + "，这趟航班的经济舱价格是" + str(flight_list[i]["cabin"][-1]["adultPrice"]) + "，出发时间是" + str(dep_time) + "，到达时间是" + str(arr_time), token)
